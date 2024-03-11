574569dd84be717fa958ab3c4e9656bd6b8c38db
Brian Keane <lonesomewhistle@gmail.com>
2024-03-11 16:50:21 -0500

    refactor tests

--- src/backend/app/tests/conftest.py
+++ src/backend/app/tests/conftest.py
@@ -3,12 +3,15 @@ from collections.abc import Generator
 import pytest
 from fastapi.testclient import TestClient
 from sqlmodel import Session, delete
+from spotipy import oauth2
+import spotipy
 
 from app.core.config import settings
 from app.core.db import engine, init_db
 from app.main import app
 from app.models import Item, User, SpotifyUser
 from app.tests.utils.user import authentication_token_from_email
+from app.tests.utils.spotify_user import random_token_info
 from app.tests.utils.utils import get_superuser_token_headers
 
 
@@ -42,3 +45,48 @@ def normal_user_token_headers(client: TestClient, db: Session) -> dict[str, str]
     return authentication_token_from_email(
         client=client, email=settings.EMAIL_TEST_USER, db=db
     )
+
+
+class MockSpotifyOauth:
+    token_info = random_token_info()
+
+    def __init__(
+        self,
+        spotify_client_id: str,
+        spotify_client_secret: str,
+        redirect_uri: str,
+        scope: str,
+    ):
+        self.spotify_client_id = spotify_client_id
+        self.spotify_client_secret = spotify_client_secret
+        self.redirect_uri = redirect_uri
+        self.scope = scope
+
+    def get_access_token(self, code: str, check_cache: bool):
+        return MockSpotifyOauth.token_info
+
+
+class MockSpotify:
+    token_info: dict = MockSpotifyOauth.token_info
+
+    def __init__(self, auth: str):
+        assert auth == MockSpotify.token_info["access_token"]
+
+    def current_user(self) -> dict:
+        return {
+            "id": "random_spotify_id",
+            "display_name": "some_spotify_display_name",
+            "email": "test@testymctesterson.com",
+        }
+
+
+@pytest.fixture(scope="function")
+def mock_SpotifyOauth(monkeypatch):
+    monkeypatch.setattr(oauth2, "SpotifyOAuth", MockSpotifyOauth)
+    return MockSpotifyOauth
+
+
+@pytest.fixture(scope="function")
+def mock_Spotify(monkeypatch):
+    monkeypatch.setattr(spotipy, "Spotify", MockSpotify)
+    return MockSpotify
--- src/backend/app/tests/test_lib.py
+++ src/backend/app/tests/test_lib.py
@@ -1,43 +1,42 @@
 from sqlmodel import Session
 
-from spotipy import oauth2, Spotify
+from spotipy import oauth2
 import spotipy
 
 from app.lib import get_or_create_spotify_user_from_spotify_code
 from app.tests.utils.spotify_user import create_random_spotify_user, random_token_info
 from app.tests.utils.user import create_random_user
+
 mocked_token_info = random_token_info()
 
+
 class MockSpotifyOauth:
-    def __init__(self, spotify_client_id: str, spotify_client_secret: str, redirect_uri: str, scope: str):
+    def __init__(
+        self,
+        spotify_client_id: str,
+        spotify_client_secret: str,
+        redirect_uri: str,
+        scope: str,
+    ):
         self.spotify_client_id = spotify_client_id
         self.spotify_client_secret = spotify_client_secret
         self.redirect_uri = redirect_uri
         self.scope = scope
 
-
-    def get_access_token(self,code: str, check_cache: bool):
+    def get_access_token(self, code: str, check_cache: bool):
         return mocked_token_info
 
-class MockSpotify:
-    def __init__(self, auth: str):
-        assert auth == mocked_token_info["access_token"]
 
-    def current_user(self) -> dict:
-        return { "id": "random_spotify_id",
-                 "display_name": "some_spotify_display_name",
-                 "email": "test@testymctesterson.com" }
-
-
-def test_get_or_create_spotify_from_code_where_spotify_user_exists(db: Session, monkeypatch):
-    monkeypatch.setattr(oauth2, "SpotifyOAuth", MockSpotifyOauth)
-    user= create_random_user(db=db)
+def test_get_or_create_spotify_from_code_where_spotify_user_exists(
+    db: Session, mock_SpotifyOauth, mock_Spotify
+):
+    user = create_random_user(db=db)
     spotify_user = create_random_spotify_user(db=db, user_id=user.id)
     spotify_user.spotify_user_id = "random_spotify_id"
     db.add(spotify_user)
     db.commit()
 
-    monkeypatch.setattr(spotipy, "Spotify", MockSpotify)
-
-    spotify_user_retrieved = get_or_create_spotify_user_from_spotify_code(session=db, code="this_is_the_code")
+    spotify_user_retrieved = get_or_create_spotify_user_from_spotify_code(
+        session=db, code="this_is_the_code"
+    )
     assert spotify_user.id == spotify_user_retrieved.id
